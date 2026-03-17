2835041d7477ca8af2024a356e47941109801842
Xavier Varghese <xaviervarghese468@gmail.com>
2026-03-17 13:45:01 +0530

    Update Selenium & Java Automation Knowledge value
    Commit

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -85,7 +85,7 @@ const ResumeSection = () => {
   const resumeHighlights: { label: string; value: number; suffix: string; link?: string }[] = [
     { label: "Years Experience", value: 1, suffix: "+", link: "/experience" },
     { label: "Projects", value: 30, suffix: "+", link: "/tested-apps" },
-    { label: "Selenium & Java Automation Knowledge", suffix: 100 },
+    { label: "Selenium & Java Automation Knowledge", value: 0, suffix: "Skill" },
     { label: "Bugs Found", value: 555, suffix: "+" },
   ];
 
